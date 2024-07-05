970115c5b23b2497096f8bedc03e83a8fe1edb8b
Viktor Asenov <79015932+ViksyAsenov@users.noreply.github.com>
2024-07-06 01:47:15 +0300

    typo in expected output comment for edge case matrix

--- matrix-shenanigans.py
+++ matrix-shenanigans.py
@@ -235,7 +235,7 @@ class TestGetAdjacentLinesIfOver5(unittest.TestCase):
 
         # Output for edgeCaseMatrix should be
         # ((0,0),(0,1),(0,2),(0,3),(0,4))
-        # ((0,4),(0,5),(0,6),(0,7),(0,7))
+        # ((0,4),(0,5),(0,6),(0,7),(0,8))
 
         result = getAdjacentLinesIfOver5(edgeCaseMatrix)
         expected = [
